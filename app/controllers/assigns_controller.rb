2d4dfaa360054c37b9cf824985ba6409bdfb7fae
“MatsumuraSaki” <massaki3156@gmail.com>
2023-11-24 11:54:30 +0900

    チームメンバーの削除は、本人かオーナーのみ実行できるように変更

--- app/controllers/assigns_controller.rb
+++ app/controllers/assigns_controller.rb
@@ -16,9 +16,11 @@ class AssignsController < ApplicationController
 
   def destroy
     assign = Assign.find(params[:id])
-    destroy_message = assign_destroy(assign, assign.user)
-
-    redirect_to team_url(params[:team_id]), notice: destroy_message
+    team = find_team(params[:team_id])
+    if assign.user == current_user || team.owner == current_user
+      destroy_message = assign_destroy(assign, assign.user)
+      redirect_to team_url(params[:team_id]), notice: destroy_message
+    end
   end
 
   private
